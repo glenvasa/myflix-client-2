f23a4d94c7a239b1cf0d46d27fe57138b6193174
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-03-14 22:26:01 -0400

    Create genre-view.js

---
+++ src/components/genre-view/genre-view.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+// import Container from 'react-bootstrap/Container';
+// import Col from 'react-bootstrap/Col';
+// import Row from 'react-bootstrap/Row';
+import "./genre-view.css";
+
+export class GenreView extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  render() {
+    const { genre } = this.props;
+
+    if (!genre) return <div className="main-view" />;
+
+    return (
+      <div className="genre-view">
+        <Card style={{ width: "45 rem" }} className="genre-card">
+          <Card.Body>
+            <Card.Title className="genre-name">{genre.Name}</Card.Title>
+            <Card.Text className="genre-description">
+              {genre.Description}
+            </Card.Text>
+          </Card.Body>
+
+          <Link to={"/"}>
+            {/* <Link to={`/movies/${movie._id}`}> */}
+            <Button
+              className="genreview-home-button"
+              style={{ background: "#690f38" }}
+            >
+              Home
+            </Button>
+          </Link>
+        </Card>
+      </div>
+    );
+  }
+}
+
+GenreView.propTypes = {
+  Genre: PropTypes.shape({
+    Name: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+  }),
+};
