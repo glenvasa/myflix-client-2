31f740a40f0895b1b3b475ccc7649824255e6e4e
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-03-15 16:35:46 -0400

    wrapped filter in a div to fix filter visiblity / card button activation problem

--- src/components/visibility-filter-input/visibility-filter-input.js
+++ src/components/visibility-filter-input/visibility-filter-input.js
@@ -9,12 +9,16 @@ import "./visibility-filter-input.css";
 
 function VisibilityFilterInput(props) {
   return (
-    <Form.Control
-      onChange={(e) => props.setFilter(e.target.value)}
-      value={props.VisibiltyFilter}
-      placeholder="Search Movies..."
-      className="visibility-filter"
-    />
+    // wrapping filter in filter-container fixed problem of having either active 
+    // card buttons and hidden nav filter or visible nav filter and inactive card buttons
+    <div className="filter-container">
+      <Form.Control
+        onChange={(e) => props.setFilter(e.target.value)}
+        value={props.VisibiltyFilter}
+        placeholder="Search Movies..."
+        className="visibility-filter"
+      />
+   </div>
   );
 }
 
