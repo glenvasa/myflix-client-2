0b036794d29c66d52095f0d453a42694ede5cca4
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-03-15 15:31:26 -0400

    changed logout button to a link; other minor changes

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,42 +1,54 @@
 import React from "react";
+// import { ModalBody } from "react-bootstrap";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+// import Button from 'react-bootstrap/Button'
 import { Link } from "react-router-dom";
 
 import "./navbar.css";
 
 function NavBar() {
+
+  
   function onLogOut() {
     localStorage.removeItem("token");
     localStorage.removeItem("user");
     window.open("/", "_self");
   }
 
+  
+
   return (
     <div>
       <Navbar collapseOnSelect expand="lg" className="fixed-top navbar-main">
         <Navbar.Brand as={Link} to="/" className="brand-myflix">
-          MyFlix!
+          MYFLIX
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link as={Link} to="/" className="navbar-link">
+          <Nav className="ml-auto">
+            {/* {  
+            
+            (document.querySelectorAll('.movies-list').length === 0)
+              ? 
+             ( <Nav.Link as={Link} to="/" className="navbar-link">
               Home
-            </Nav.Link>
+            </Nav.Link>)
+            : ''
+            } */}
+            
             <Nav.Link as={Link} to="/users/:Username" className="navbar-link">
               Profile
             </Nav.Link>
-          </Nav>
-          <button
+            <Nav.Link
             onClick={onLogOut}
-            variant="dark"
-            type="submit"
-            className="button log-out-button"
+            className="navbar-link log-out"
           >
             {" "}
-            Log Out
-          </button>
+            Logout
+          </Nav.Link>
+          </Nav>
+          
         </Navbar.Collapse>
       </Navbar>
     </div>
