8244d5ddd1552beb3568a3690ec222597b02f661
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-03-14 22:27:00 -0400

    Create movies-list.js

---
+++ src/components/movies-list/movies-list.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import VisibilityFilterInput from "../visibility-filter-input/visibility-filter-input";
+import { MovieCard } from "../movie-card/movie-card";
+
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import NavBar from "../navbar/navbar";
+
+const mapStateToProps = (state) => {
+  const { visibilityFilter } = state;
+  return { visibilityFilter };
+};
+
+function MoviesList(props) {
+  const { movies, visibilityFilter } = props;
+  let filteredMovies = movies;
+
+  if (visibilityFilter !== "") {
+    filteredMovies = movies.filter((m) =>
+      m.Title.toLowerCase().includes(visibilityFilter.toLowerCase())
+    );
+  }
+
+  if (!movies) return <div className="main-view" />;
+
+  return (
+    <>
+      <NavBar />
+      <VisibilityFilterInput visibilityFilter={visibilityFilter} />
+      <div className="movies-list p-2">
+        {/* <Container> */}
+        <Row>
+          <Col className="d-flex flex-wrap justify-content-around">
+            {filteredMovies.map((m) => (
+              <MovieCard key={m._id} movie={m} />
+            ))}
+          </Col>
+        </Row>
+        {/* </Container> */}
+      </div>
+    </>
+  );
+}
+
+export default connect(mapStateToProps)(MoviesList);
