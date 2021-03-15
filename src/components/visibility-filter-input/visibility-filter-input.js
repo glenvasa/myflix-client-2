8f75d8af1652d28f01d1c1cc6af06043bd77142d
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-03-14 22:27:51 -0400

    Create visibility-filter-input.js

---
+++ src/components/visibility-filter-input/visibility-filter-input.js
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+import Form from "react-bootstrap/Form";
+
+import { setFilter } from "../../actions/actions";
+import "./visibility-filter-input.css";
+
+function VisibilityFilterInput(props) {
+  return (
+    <Form.Control
+      onChange={(e) => props.setFilter(e.target.value)}
+      value={props.VisibiltyFilter}
+      placeholder="Search Movies"
+      className="visibility-filter"
+    />
+  );
+}
+
+export default connect(null, { setFilter })(VisibilityFilterInput);
+
+VisibilityFilterInput.propTypes = {
+  visibilityFilter: PropTypes.string,
+};
