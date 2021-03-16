576cb08cd24077cd2e4706f912099b701218a2ab
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-03-16 14:06:27 -0400

    Update director-view.js

--- src/components/director-view/director-view.js
+++ src/components/director-view/director-view.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 import PropTypes from "prop-types";
-import NavBar from '../navbar/navbar'
+import NavBar from "../navbar/navbar";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 // import Container from 'react-bootstrap/Container';
 // import Col from 'react-bootstrap/Col';
 // import Row from 'react-bootstrap/Row';
 import "./director-view.css";
-import { Navbar } from "react-bootstrap";
 
 export class DirectorView extends React.Component {
   constructor() {
@@ -22,29 +22,28 @@ export class DirectorView extends React.Component {
 
     if (!director) return <div className="main-view" />;
 
+    let id = localStorage.getItem("id");
+
     return (
       <>
-      <NavBar/>
-      <div className="director-view">
-        <Card style={{ width: "45 rem" }} className="director-card">
-          <Card.Body>
-            <Card.Title className="dir-name">{director.Name}</Card.Title>
-            <Card.Text className="dir-born">Born: {director.Birth}</Card.Text>
-            <Card.Text className="dir-died">Died: {director.Death}</Card.Text>
-            <Card.Text className="dir-bio">Bio: {director.Bio}</Card.Text>
-          </Card.Body>
-          <Link to={"/"}>
-            {/* <Link to={`/movies/${movie._id}`}> */}
-            <Button
-              variant="link"
-              className="dirview-home-button"
-              style={{ background: "#690f38" }}
-            >
-              Home
-            </Button>
-          </Link>
-        </Card>
-      </div>
+        <NavBar />
+        <div className="director-view">
+          <Card style={{ width: "45 rem" }} className="director-card">
+            <Card.Body className="dir-card-body">
+              <Card.Title className="dir-name">{director.Name}</Card.Title>
+              <Card.Text className="dir-bio">{director.Bio}</Card.Text>
+              
+              <Card.Text className="dir-born">Born: {director.Birth}</Card.Text>
+              <Card.Text className="dir-died">Died: {director.Death}</Card.Text>
+            </Card.Body>
+            {/* <Link to={"/"}> */}
+            <div className="dirview-button-container">
+              <Link to={`/movies/${id}`}>
+                <Button className="dirview-back-button">Back</Button>
+              </Link>
+            </div>
+          </Card>
+        </div>
       </>
     );
   }
